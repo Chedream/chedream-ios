9730e3a68094b7680dd45502557a04c5dcf3250a
Andrews <andrew.sirenko.ck@gmail.com>
2015-03-11 00:44:56 +0200

    first commit

---
+++ chedream-ios/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  chedream-ios
+//
+//  Created by Andrews on 11.03.15.
+//  Copyright (c) 2015 Chedream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
