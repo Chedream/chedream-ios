7e2b4083d9f479b3dba4acc421eec0b2fef6aa0d
Andrews <andrew.sirenko.ck@gmail.com>
2015-03-30 00:51:34 +0300

    dream poster link generation

--- chedream-ios/Dream.m
+++ chedream-ios/Dream.m
@@ -10,4 +10,15 @@
 
 @implementation Dream
 
+- (instancetype)initWithDictionary:(NSDictionary *)dict {
+    if ((self = [super init])) {
+        self.dreamId = dict[@"id"];
+        self.title = dict[@"title"];
+        self.slug = dict[@"slug"];
+        self.imageLink = [NSString stringWithFormat:@"%@%@", @"http://chedream.org/upload/media/poster/0001/01/", dict[@"media_poster"][@"provider_reference"]];
+    }
+    
+    return self;
+}
+
 @end
